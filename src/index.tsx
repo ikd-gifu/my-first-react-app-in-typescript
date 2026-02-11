da893e9e11528e961a3b57bdc9478616aad782ac
ikd-gifu <oki19ki@gmail.com>
2026-02-11 09:27:00 +0900

    ss11 68 FC入門

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App'; // コンポーネント。HTMLを表示するためのもの
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// type Foo = JSX.IntrinsicAttributes;
+    // interface Attributes { // JSXで使う属性の型を定義するインターフェース
+    //     key?: Key;
+    // }
+
+// propsでコンポーネントに属性を渡す
+// 文字列リテラル以外は{}で囲み、式として渡す。JSの値を渡せる
+ReactDOM.render(<App message="Hello, world!" />, document.getElementById('root'));
+// 'root'というIDを持つHTML要素に、<App />コンポーネントによって生成されたHTMLを差し込む
+// public/index.html内にある<div id="root"></div>が対象
